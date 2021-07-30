4642e6f9243c9923a7ce827c5a809f74ccfaf824
taryune <tarumi@arshub.io>
2021-07-30 13:52:02 +0900

    add pause

--- contracts/ERC721WithRoyalities.sol
+++ contracts/ERC721WithRoyalities.sol
@@ -5,6 +5,7 @@ import 'hardhat/console.sol';
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/access/AccessControlEnumerable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
@@ -17,6 +18,7 @@ contract ERC721WithRoyalities is
     AccessControlEnumerable,
     ERC721Enumerable,
     ERC721Burnable,
+    ERC721Pausable,
     ERC721URIStorage,
     ERC2981Royalties
 {
@@ -91,7 +93,7 @@ contract ERC721WithRoyalities is
         address from,
         address to,
         uint256 tokenId
-    ) internal virtual override(ERC721, ERC721Enumerable) {
+    ) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
