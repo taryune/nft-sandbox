b88d99b811f8b312c58d1c3eeced9e88eeb25613
taryune <taryunelab@gmail.com>
2021-07-31 12:48:33 +0900

    clean clean

--- contracts/ERC721WithRoyalitiesMetaTx.sol
+++ contracts/ERC721WithRoyalitiesMetaTx.sol
@@ -13,124 +13,22 @@ import '@openzeppelin/contracts/utils/Context.sol';
 
 import './ERC2981Royalties.sol';
 import './ERC2771Context.sol';
+import './ERC721WithRoyalities.sol';
 
 contract ERC721WithRoyalitiesMetaTx is
     Context,
-    AccessControlEnumerable,
-    ERC721Enumerable,
-    ERC721Burnable,
-    ERC721Pausable,
-    ERC721URIStorage,
-    ERC2981Royalties,
+    ERC721WithRoyalities,
     ERC2771Context
 {
-    using Strings for uint256;
-    using Counters for Counters.Counter;
-
-    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
-    bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
-
-    Counters.Counter private _tokenIdTracker;
-
-    string private _baseTokenURI;
-
     constructor(
         string memory name,
         string memory symbol,
         string memory baseTokenURI,
         MinimalForwarder fowarder
-    ) ERC721(name, symbol) ERC2771Context(address(fowarder)) {
-        _baseTokenURI = baseTokenURI;
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _setupRole(MINTER_ROLE, _msgSender());
-        _setupRole(PAUSER_ROLE, _msgSender());
-    }
-
-    function mint(address to, string memory _tokenURI)
-        external
-        returns (uint256)
-    {
-        require(
-            hasRole(MINTER_ROLE, _msgSender()),
-            'ERC721WithRoyalities: must have minter role to mint'
-        );
-        _tokenIdTracker.increment();
-        _safeMint(to, _tokenIdTracker.current());
-        _setTokenURI(_tokenIdTracker.current(), _tokenURI);
-        return _tokenIdTracker.current();
-    }
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function setTokenRoyalty(
-        uint256 tokenId,
-        address recipient,
-        uint256 value
-    ) external {
-        require(
-            hasRole(MINTER_ROLE, _msgSender()),
-            'ERC721WithRoyalities: must have minter role to set royalty'
-        );
-        require(
-            tokenId <= _tokenIdTracker.current(),
-            'ERC721WithRoyalities: invalid token id'
-        );
-        _setTokenRoyalty(tokenId, recipient, value);
-    }
-
-    function pause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            'ERC721WithRoyalities: must have pauser role to pause'
-        );
-        _pause();
-    }
-
-    function unpause() public virtual {
-        require(
-            hasRole(PAUSER_ROLE, _msgSender()),
-            'ERC721WithRoyalities: must have pauser role to unpause'
-        );
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(
-            AccessControlEnumerable,
-            ERC721,
-            ERC721Enumerable,
-            ERC2981Royalties
-        )
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+    )
+        ERC721WithRoyalities(name, symbol, baseTokenURI)
+        ERC2771Context(address(fowarder))
+    {}
 
     function _msgSender()
         internal
---
+++ test/erc721withRoyality-test.ts
@@ -0,0 +1,164 @@
+/* eslint-env mocha */
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import {
+  // eslint-disable-next-line
+  ERC721WithRoyalities__factory,
+  ERC721WithRoyalities,
+} from '../typechain';
+
+describe('ERC721RoyalitiesMetaTx', () => {
+  let contract: ERC721WithRoyalities;
+  let signers: any;
+
+  beforeEach(async () => {
+    const [deployer, user] = await ethers.getSigners();
+    signers = { deployer, user };
+    const ERC721WithRoyalitiesFactory = await ethers.getContractFactory(
+      'ERC721WithRoyalities',
+      deployer
+    );
+
+    const name = 'Nameko';
+    const symbol = 'NMK';
+    const baseTokenURI = 'http://localhost:3000/';
+
+    const erc721WithRoyalities = await ERC721WithRoyalitiesFactory.deploy(
+      name,
+      symbol,
+      baseTokenURI
+    );
+    contract = new ERC721WithRoyalities__factory(deployer).attach(
+      erc721WithRoyalities.address
+    );
+  });
+
+  describe('Mint', async () => {
+    it('successful', async () => {
+      await contract.mint(signers.deployer.address, 'hogehoge');
+      expect(await contract.ownerOf(1)).to.be.equal(signers.deployer.address);
+      expect(await contract.tokenURI(1)).to.be.equal('hogehoge');
+      expect(await contract.balanceOf(signers.deployer.address)).to.be.eq(1);
+    });
+    it('must have minter role', async () => {
+      await expect(
+        contract.connect(signers.user).mint(signers.user.address, 'fugafuga')
+      ).to.revertedWith('ERC721WithRoyalities: must have minter role to mint');
+    });
+  });
+
+  describe('Transfer', async () => {
+    beforeEach(async () => {
+      await contract.mint(signers.deployer.address, 'hogehoge');
+      expect(await contract.ownerOf(1)).to.be.equal(signers.deployer.address);
+    });
+    it('deployer to user', async () => {
+      await contract.transferFrom(
+        signers.deployer.address,
+        signers.user.address,
+        1
+      );
+      expect(await contract.ownerOf(1)).to.be.equal(signers.user.address);
+    });
+
+    it('transfer of token that is not own', async () => {
+      await expect(
+        contract.transferFrom(signers.user.address, signers.deployer.address, 1)
+      ).to.revertedWith('ERC721: transfer of token that is not own');
+    });
+  });
+
+  describe('Burn', async () => {
+    beforeEach(async () => {
+      await contract.mint(signers.deployer.address, 'hogehoge');
+      expect(await contract.ownerOf(1)).to.be.equal(signers.deployer.address);
+    });
+    it('successful', async () => {
+      await contract.burn(1);
+      expect(await contract.balanceOf(signers.deployer.address)).to.be.equal(0);
+      await expect(contract.ownerOf(1)).to.revertedWith(
+        'ERC721: owner query for nonexistent token'
+      );
+    });
+    it('transfer and burn', async () => {
+      await contract.transferFrom(
+        signers.deployer.address,
+        signers.user.address,
+        1
+      );
+      expect(await contract.balanceOf(signers.user.address)).to.be.equal(1);
+      await contract.connect(signers.user).burn(1);
+      await expect(contract.ownerOf(1)).to.revertedWith(
+        'ERC721: owner query for nonexistent token'
+      );
+    });
+
+    it('caller is not owner', async () => {
+      await expect(contract.connect(signers.user).burn(1)).to.revertedWith(
+        'ERC721Burnable: caller is not owner nor approved'
+      );
+    });
+  });
+
+  describe('Royalty', async () => {
+    beforeEach(async () => {
+      await contract.mint(signers.deployer.address, 'hogehoge');
+    });
+    it('successful', async () => {
+      // 50.00%
+      await contract.setTokenRoyalty(1, signers.deployer.address, 5000);
+      const info = await contract.royaltyInfo(1, 10);
+      expect(info[0]).to.be.equal(signers.deployer.address);
+      // 10 * 5000 / 10000 = 5
+      expect(info[1].toNumber()).to.be.equal(5);
+    });
+    it('must have minter role', async () => {
+      await expect(
+        contract
+          .connect(signers.user)
+          .setTokenRoyalty(1, signers.user.address, 5000)
+      ).to.revertedWith(
+        'ERC721WithRoyalities: must have minter role to set royalty'
+      );
+    });
+    it('too high', async () => {
+      await expect(
+        // 100.01%
+        contract.setTokenRoyalty(1, signers.deployer.address, 10001)
+      ).to.revertedWith('ERC2981Royalties: too high');
+    });
+    it('invalid token id', async () => {
+      await expect(
+        contract.setTokenRoyalty(2, signers.deployer.address, 5000)
+      ).to.revertedWith('ERC721WithRoyalities: invalid token id');
+    });
+  });
+  describe('Pause', async () => {
+    it('successful', async () => {
+      await contract.pause();
+      await expect(
+        contract.mint(signers.deployer.address, 'hoge')
+      ).to.revertedWith('ERC721Pausable: token transfer while paused');
+    });
+    it('must have pauser role', async () => {
+      await expect(contract.connect(signers.user).pause()).to.revertedWith(
+        'ERC721WithRoyalities: must have pauser role to pause'
+      );
+    });
+  });
+  describe('Unpause', async () => {
+    beforeEach(async () => {
+      await contract.pause();
+    });
+    it('successful', async () => {
+      await contract.unpause();
+      await contract.mint(signers.deployer.address, 'hogehoge');
+      expect(await contract.ownerOf(1)).to.be.equal(signers.deployer.address);
+    });
+    it('must have pauser role', async () => {
+      await expect(contract.connect(signers.user).unpause()).to.revertedWith(
+        'ERC721WithRoyalities: must have pauser role to unpause'
+      );
+    });
+  });
+});
